8d9afb3bd3f6e6e44f5d92e15bb84cd0efff7d8a
aleksem07 <aleksem07@gmail.com>
2024-01-31 01:52:52 +0300

    feat: add app and exit module

--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
 import './welcome.js';
+import './app.js';
--- src/welcome.js
+++ src/welcome.js
@@ -4,11 +4,11 @@ const welcome = async () => {
   const name = userName();
   
   if (name) {
-    process.stdout.write(`Welcome to the File Manager, ${name.toString()}!`);
-    process.stdout.write('\n')
+    process.stdout.write(`Welcome to the File Manager, ${name.toString()}!\n`);
+    process.stdout.write(`Press 'ctrl+c' or sent '.exit' command to exit\n`);
+    process.stdout.write(`Please, enter the command...\n`);
   } else {
-    process.stdout.write(`Welcome to the File Manager, Student1!`);
-    process.stdout.write('\n')
+    process.stdout.write(`Welcome to the File Manager, Student1!\n`);
   }
 }
 
